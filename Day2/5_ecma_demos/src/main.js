1c967cad00d86d91a142dc4bc582bb753ac7f8c7
Manish Sharma <developer@technizeredge.com>
2025-10-29 16:59:56 +0530

    All Code Snippets of JS

--- Day2/5_ecma_demos/src/main.js
+++ Day2/5_ecma_demos/src/main.js
@@ -9,5 +9,22 @@
 // import './1_datatypes/4_datatypes';
 // import './1_datatypes/5_es6_symbol';
 
-import './2_functions/1_cr_fns';
-// import './2_functions/2_fn_parameters'
+// import './2_functions/1_cr_fns';
+// import './2_functions/1.1_fn_parameters';
+// import './2_functions/2_rest_and_spread';
+// import './2_functions/3_pure_impure';
+// import './2_functions/4_fn_as_arguments';
+// import './2_functions/5_fn_context';
+// import './2_functions/6_closures';
+// import './2_functions/7_fn_currying';
+// import './2_functions/8_hof';
+
+// import './3_objects/1_custom_types';
+// import './3_objects/2_using_prototype';
+// import './3_objects/3_class';
+// import './3_objects/4_inheritance';
+
+// import './4_modules/usage';
+
+import './5_async/dom-handler';
+
